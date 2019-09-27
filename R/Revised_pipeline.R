969ce05390a430384711efbde715e59e49c77c75
Chia-Yu <jessica821112@gmail.com>
2019-09-27 21:52:40 +0200

    small changes

--- R/Revised_pipeline.R
+++ R/Revised_pipeline.R
@@ -92,7 +92,9 @@ Ps_ori <- Ps
 Ps <- Ps[ , -unique(col_NA)]
 mode(Ps) <- "numeric"
 
-
+# Do FDR correction (correct for the number of features)
+#adjust_fun <- function(x) p.adjust(p = x, method = "fdr", n = N)
+#Ps_fdr <- apply(X = Ps, MARGIN = 2, FUN = adjust_fun)
 
 # Extract the selected factors whose p value < 0.05
 # Make an empty list first
@@ -110,13 +112,7 @@ for (i in 1:N) {
 }
 names(sel_fac) <- variables
 
-
 # Then do the model test (lrtest)
-
-#######################Testing##########################
-
-
-
 # Make a empty list first
 Ps_model <- c()
 
@@ -126,21 +122,21 @@ for (i in 1:N) {
   subdata <- subset(melt_data, variable == aVariable)
   ps_lm <- c()
   if (length(sel_fac[[i]]) >= 2) {
-  pairs <- combn(x = sel_fac[[i]], m = 2)
-  for (k in 1:length(sel_fac[[i]])) {
-    for (m in 1:ncol(pairs)) {
-      if (sel_fac[[i]][k] %in% pairs[ , m] ) {
-        # setdiff(pairs[ , m], sel_fac[[i]][k]) finds the element of pairs[ , m] in sel_fac[[1]][1]
-        fmla1 <- as.formula(paste("rank(value) ~ ", paste((setdiff(pairs[ , m], sel_fac[[i]][k])), collapse= "+")))
-        fmla2 <- as.formula(paste("rank(value) ~ ", paste(pairs[ , m], collapse= "+")))
-        m1 <- lm(data = subdata, fmla1)
-        m2 <- lm(data = subdata, fmla2)
-        p_lm <- lmtest::lrtest (m1, m2)$"Pr(>Chisq)"[2]
-        names(p_lm) <- paste(sel_fac[[i]][k])
-        ps_lm <- c(ps_lm, p_lm)
+    pairs <- combn(x = sel_fac[[i]], m = 2)
+    for (k in 1:length(sel_fac[[i]])) {
+      for (m in 1:ncol(pairs)) {
+        if (sel_fac[[i]][k] %in% pairs[ , m] ) {
+          # setdiff(pairs[ , m], sel_fac[[i]][k]) finds the element of pairs[ , m] in sel_fac[[1]][1]
+          fmla1 <- as.formula(paste("rank(value) ~ ", paste((setdiff(pairs[ , m], sel_fac[[i]][k])), collapse= "+")))
+          fmla2 <- as.formula(paste("rank(value) ~ ", paste(pairs[ , m], collapse= "+")))
+          m1 <- lm(data = subdata, fmla1)
+          m2 <- lm(data = subdata, fmla2)
+          p_lm <- lmtest::lrtest (m1, m2)$"Pr(>Chisq)"[2]
+          names(p_lm) <- paste(sel_fac[[i]][k])
+          ps_lm <- c(ps_lm, p_lm)
+        }
       }
     }
-  }
   } else if (length(sel_fac[[i]]) == 1){
     fmla3 <- as.formula(paste("value ~ ", colnames(subdata), sep = ""))
     ps_lm <- as.list(kruskal.test(fmla3 , data = subdata))$p.value
@@ -154,7 +150,22 @@ for (i in 1:N) {
 names(Ps_model) <- variables
 
 
+
+
+#######################Testing##########################
+
 # Do FDR correction (correct for the number of features)
 adjust_fun <- function(x) p.adjust(p = x, method = "fdr", n = N)
 Ps_fdr <- apply(X = Ps, MARGIN = 2, FUN = adjust_fun)
+
+adjust_fun <- function(x) p.adjust(p = x, method = "fdr")
+adjust_fun(Ps_model[[1]][1])
+adjust_fun(Ps_model[[1]])
+Ps_fdr <- lapply(X = Ps_model, FUN = adjust_fun)
+
+
+
+
+
+
 
